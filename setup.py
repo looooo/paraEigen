afb994c3b3bc11342ba34821067835115b261c3c
looooo <sppedflyer@gmail.com>
2016-03-17 15:20:55 +0100

    add distutils installer

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup, Extension
+from distutils import sysconfig
+import sys
+
+include_dirs = ["/usr/include/eigen3/"]
+
+setup(name='eigen',
+      version='0.0.1',
+      author='Loooo',
+      author_email='sppedflyer@gmail.com',
+      url="https://github.com/looooo/py-eigen",
+      description='Wrap parts of Eigen3 using pybind11',
+      ext_modules=[Extension('eigen',
+                   sources=['python_eigen.cpp'],
+                   include_dirs=include_dirs,
+                   extra_compile_args=['-std=c++11'])])
